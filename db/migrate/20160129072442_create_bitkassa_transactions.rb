51b130d4efa6e5cf9bd80e08dc5874a12c854e32
Bèr Kessels <ber@berk.es>
2016-02-15 14:00:44 +0100

    fixup! Add Spree::BitkassaTransaction model to capture transaction details.

--- db/migrate/20160129072442_create_bitkassa_transactions.rb
+++ db/migrate/20160129072442_create_bitkassa_transactions.rb
@@ -2,7 +2,7 @@ class CreateBitkassaTransactions < ActiveRecord::Migration
   def change
     create_table :spree_bitkassa_transactions do |t|
       t.references :spree_payment, index: true
-      t.string :bitkassa_payment_id
+      t.string :payment_id
       t.string :address
       t.integer :amount
       t.integer :expire
