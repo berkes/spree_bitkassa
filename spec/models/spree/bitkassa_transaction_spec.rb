4fe1b349ea71be2889f0db57d3dca0c930655fe5
Bèr Kessels <ber@berk.es>
2016-02-19 08:46:28 +0100

    Ensure transactions can access the order (via the payment)

--- spec/models/spree/bitkassa_transaction_spec.rb
+++ spec/models/spree/bitkassa_transaction_spec.rb
@@ -5,4 +5,18 @@ describe Spree::BitkassaTransaction do
     subject.build_payment
     expect(subject.payment).to be_a Spree::Payment
   end
+
+  describe "#order" do
+    it "is nil when there is no payment" do
+      subject.payment = nil
+      expect(subject.order).to be_nil
+    end
+
+    it "is the order from the payment" do
+      order = double(:order)
+      allow(subject).to receive(:payment).
+        and_return(double(:payment, order: order))
+      expect(subject.order).to be order
+    end
+  end
 end
