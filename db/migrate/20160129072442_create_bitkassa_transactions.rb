b4432c4c900f40644b63f99befd82476d1f6a41c
Bèr Kessels <ber@berk.es>
2016-03-10 08:16:32 +0100

    Adds index to the transaction bitkassa_payment_id
    We have to find by this id on callbacks and therefore it has to be
    indexed

--- db/migrate/20160129072442_create_bitkassa_transactions.rb
+++ db/migrate/20160129072442_create_bitkassa_transactions.rb
@@ -2,7 +2,7 @@ class CreateBitkassaTransactions < ActiveRecord::Migration
   def change
     create_table :spree_bitkassa_transactions do |t|
       t.references :payment, index: true
-      t.string :bitkassa_payment_id
+      t.string :bitkassa_payment_id, index: true
       t.string :address
       t.integer :amount
       t.integer :expire
